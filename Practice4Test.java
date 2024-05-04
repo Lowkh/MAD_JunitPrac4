5e8205c39dbf3d954b7ceb70f8a42e8237906217
Low <54532616+Lowkh@users.noreply.github.com>
2024-05-04 14:31:42 +0800

    Update Practice4Test.java

--- Practice4Test.java
+++ Practice4Test.java
@@ -9,8 +9,9 @@ import java.util.ArrayList;
 public class UserTest{
     private User user;
     private UserAdapter userAdapter;
+
     @Test
-    public void testUser(){
+    public void testForUserAdapterClass(){
         user = new User("John Doe", "MAD Developer", 1, false);
 
         ArrayList<User> test_List = new ArrayList<>();
@@ -21,7 +22,11 @@ public class UserTest{
         test_List.add(user);
 
         userAdapter = new UserAdapter(test_List,null);
-        assertEquals(4, userAdapter.getItemCount());
+        assertEquals(5, userAdapter.getItemCount());
+    }
+    @Test
+    public void testForUserClass(){
+        user = new User("John Doe", "MAD Developer", 1, false);
 
         assertEquals("John Doe", user.getName());
         assertEquals("MAD Developer", user.getDescription());
